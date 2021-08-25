007230cd9bbcc428cbac96767761a74ce0361a30
Pranav <pranavruparel714@gmail.com>
2021-08-25 16:10:26 +0530

    map changed to image slider on index screen

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,5 @@
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(index);
-    } else {
-        // get coordinates from address
-        // then call index by using the coordinates
-
-    }
-}
-
-function index(position) {
-    var x = position.coords.latitude + "," + position.coords.longitude;
-    var link = "https://maps.google.com/?q=" + x + "&z=13&output=embed";
-    document.getElementById('map').setAttribute('src', link);
-
-    /*
-        var str = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + x + '&radius=20000&type=restaurant&key=' + key
-    
-        $.ajax({
-            url: '/places',
-            type: 'POST',
-            data: {
-                str,
-            },
-        }).done(function (ret) {
-            console.log(ret)
-        })
-    */
-
-}
-
 $(document).ready(function () {
-    getLocation();
+    $('[name="carousel-image"]').click(function () {
+        window.location.href='/menu'
+    })
 })
